fbc4419431533209f27d0b3ecbdee7e42e9b1a57
Vander-Reis <vanderreis2017@outlook.com>
2022-07-14 14:32:57 -0300

    created page profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,56 @@
+import { Container, Form, Avatar } from './styles';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import {FiArrowLeft , FiMail, FiLock , FiUser, FiCamera} from 'react-icons/fi';
+
+export function Profile() {
+    return(
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft/>
+                </a>
+            </header>
+
+            <Form>
+
+                <Avatar>
+                    <img src="https://github.com/Vander-Reis.png" alt="" />
+
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        
+                        <input type="file" id="avatar" />
+                    </label>
+                </Avatar>
+                
+                <Input
+                type="text"
+                placeholder="User"
+                icon={FiUser}
+                />
+
+                <Input
+                type="text"
+                placeholder="E-mail"
+                icon={FiMail}
+                />
+
+                <Input
+                type="password"
+                placeholder="Senha atual"
+                icon={FiLock}
+                />
+
+                <Input
+                type="password"
+                placeholder="Nova senha"
+                icon={FiLock}
+                />
+
+                <Button title="Salvar"/>
+            </Form>
+        </Container>
+    );
+}
